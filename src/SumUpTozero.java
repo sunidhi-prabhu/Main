474ae40fdd8e90081134bc0d7fff890f5c27385a
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2023-08-25 21:12:36 +0530

    initial commit

--- src/SumUpTozero.java
+++ src/SumUpTozero.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class SumUpTozero {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("enter the total number of ele an should have to sum to zero");
+        int a = in.nextInt();
+        int[] arr = new int[a];
+        int b = 1;
 
+        for (int i = 0; i < a/2; i++) {
+            arr[i]= b;
+            b++;
+        }
+        b=0;
+        for (int i = a/2; i < a; i++) {
+            arr[i] = -b;
+            b++;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
