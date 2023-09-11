3a5b0f9fa6485eb32ecea90945d43833ddca2770
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2023-09-11 18:41:22 +0530

    initial commit

---
+++ src/NoOfDigit.java
@@ -0,0 +1,8 @@
+public class NoOfDigit {
+    public static void main(String[] args) {
+         int n = 10;
+         int base = 2;
+         int ans = (int) (Math.log(n)/(Math.log(base)) + 1);
+        System.out.println(ans);
+    }
+}
