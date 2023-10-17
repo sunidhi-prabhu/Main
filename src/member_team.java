3e48239e6a03b3bf3ac21fd5f31eeb104c02c8b5
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2023-10-17 21:44:23 +0530

    initial commit

--- src/member_team.java
+++ src/member_team.java
@@ -1,26 +1,37 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class member_team {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n , m;
+        float avg;
         System.out.println("enter the total number of teams");
         n = in.nextInt();
         System.out.println("enter the total number of members in each team");
         m = in.nextInt();
         in.nextLine();
-        String[][][] arr = new String[n][m][2];
+        String[][][] arr = new String[n][m][3];
         for (int i = 0; i < n; i++) {
             System.out.println("enter the team name");
             String team = in.nextLine();
             for (int j = 0; j < m; j++) {
                 System.out.println("enter  the team member");
                 String member = in.nextLine();
+                System.out.println("enter batting avg");
+                avg = in.nextFloat();
+                in.nextLine();
+
+
+
                 arr[i][j][0] = team;
                 arr[i][j][1] = member;
+                arr[i][j][2] = String.valueOf(avg);
             }
         }
-        System.out.println(Arrays.deepToString(arr));
+        for (String[][] teamArray : arr) {
+            for (String[] memberInfo : teamArray) {
+                System.out.println("Team: " + memberInfo[0] + ", Member: " + memberInfo[1] + ", Batting Avg: " + memberInfo[2]);
+            }
+        }
     }
 }
