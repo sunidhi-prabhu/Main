fee1a48cb356a410d21ee64b0a73b337e697f91d
sunidhi prabhu <sunidhiprabhu08@gmail.com>
2023-11-15 19:54:23 +0530

    initial commit

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,3 +1,8 @@
+//Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+// find two numbers such that they add up to a specific target number. using binary search
+
+
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -5,17 +10,22 @@ public class TwoSum {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.println("enter size");
+        System.out.println("enter the size");
         int n = in.nextInt();
+
+
         int[] arr = new int[n];
-        System.out.println("enter the ele of array");
+        System.out.println("enter the elements of array");
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        //int[] sum = new int[n];
         System.out.println(Arrays.toString(arr));
+
+
         System.out.println("enter the number to check sum");
         int k = in.nextInt();
+
+
         int low = 0;
         int high = n-1;
         while(low <= high){
@@ -23,18 +33,18 @@ public class TwoSum {
             if(sum == k) {
                 System.out.println("[" + arr[low] + "," + arr[high] + "]");
                 return;
+            }
 
-            } else if (sum < k) {
+            else if (sum < k)
                 low ++;
 
-            }else{
+            else
                 high --;
-            }
 
         }
 
-            }
-        }
+    }
+}
 
 
 
